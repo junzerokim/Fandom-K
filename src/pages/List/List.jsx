bd1f26cb55db3953049ce7ec8779e9fc3df9130f
Minjo Kwawk <iclock1960@naver.com>
2024-07-15 21:46:15 +0900

    feat : 크레딧 ui 구현

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../../components/Header';
 import DonationsList from './components/DonationList';
-import ThisMonthsChart from '../../components/ThisMonthsChart';
+import ThisMonthsChart from '../components/ThisMonthsChart';
+import MyCredit from '../components/MyCredit';
 
 function List() {
   return (
     <>
       <Header />
+      <MyCredit />
       <DonationsList />
       <ThisMonthsChart />
     </>

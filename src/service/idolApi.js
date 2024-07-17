069be4009ed36374b7aaec050f637053edc77a07
nayoonju <bubblenana@bubblecon.io>
2024-07-17 22:58:44 +0900

    feat: 더보기 기능 및 리사이즈에 따른 페이지 사이즈 변경

--- src/service/idolApi.js
+++ src/service/idolApi.js
@@ -1,22 +1,20 @@
-const BASE_URL = 'https://fandom-k-api.vercel.app/8-3';
+const BASE_URL = "https://fandom-k-api.vercel.app/8-3";
 
-export default async function getIdolChart() {
-  const LIMIT = 10;
-  const url = `${BASE_URL}/charts/gender?gender=female&pageSize=${LIMIT}`;
-
+export default async function getIdolChart(gender = "female", pageSize = 10) {
+  const url = `${BASE_URL}/charts/gender?gender=${gender}&pageSize=${pageSize}`;
   try {
     const response = await fetch(url);
 
     if (!response || !response.ok) {
-      console.error('Fetch failed:', response.status, response.statusText);
+      console.error("Fetch failed:", response.status, response.statusText);
       return [];
     }
     const result = await response.json();
     const data = result.idols;
-
+    console.log(data);
     return data;
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error("Error fetching data:", error);
     return [];
   }
 }

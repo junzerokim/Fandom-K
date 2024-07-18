c8dea4d40856a31f27cea66b316a20a63b92072c
Jeongmin Oh <ojm51@naver.com>
2024-07-18 15:16:08 +0900

    relocate: 폴더 구조 수정

--- src/pages/List/components/CreditModal/CreditModal.jsx
+++ src/pages/List/components/CreditModal/CreditModal.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
-import './CreditModal.css';
-import CreditIcon from '../assets/images/ico_credit.png';
-import CreditWhiteIcon from '../assets/images/ico_credit.svg';
+import { useState, useContext } from "react";
 
-function CreditModal({ onClose, onRecharge }) {
+import "./CreditModal.css";
+import CreditIcon from "../../../../assets/images/ico_credit.png";
+import CreditWhiteIcon from "../../../../assets/images/ico_credit.svg";
+import { CreditContext } from "../../../../components/CreditContextProvider";
+
+function CreditModal({ onClose }) {
+  const { handleRecharge } = useContext(CreditContext);
+
   const [selectedCredit, setSelectedCredit] = useState(100);
 
   const creditOptions = [100, 500, 1000];
@@ -13,9 +17,6 @@ function CreditModal({ onClose, onRecharge }) {
       <div className="modal">
         <div className="modal-header">
           <h2>크레딧 충전하기</h2>
-          <button className="close-btn" onClick={onClose}>
-            X
-          </button>
           <button className="close-btn" onClick={onClose}>
             X
           </button>
@@ -44,7 +45,6 @@ function CreditModal({ onClose, onRecharge }) {
           <button onClick={() => handleRecharge(selectedCredit)}>
             <img src={CreditWhiteIcon} alt="Credit Icon" />
             충전하기
-          </button>
           </button>
         </div>
       </div>
